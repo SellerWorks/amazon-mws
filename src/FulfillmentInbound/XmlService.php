337ed5f953b0183a1e1bbb73417d94076ae10e39
Steven Nebes <snebes@gmail.com>
2016-05-19 12:53:53 -0500

    init

---
+++ src/FulfillmentInbound/XmlService.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SellerWorks\Amazon\MWS\FulfillmentInbound;
+
+/**
+ */
+class XmlService extends \Sabre\Xml\Service
+{
+    /**
+     */
+    public function __construct()
+    {
+        
+    }
+}
