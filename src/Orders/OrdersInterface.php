918d731b24776c02576ff1ae8aed47b61ff9b68f
Steve Nebes <snebes@gmail.com>
2016-06-17 12:42:52 -0500

    orders

---
+++ src/Orders/OrdersInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SellerWorks\Amazon\MWS\FulfillmentInbound;
+
+use SellerWorks\Amazon\MWS\Common\RecordIterator;
+use SellerWorks\Amazon\MWS\Common\Results\GetServiceStatusResult;
+use SellerWorks\Amazon\MWS\Common\Passport;
+
+/**
+ * Amazon MWS Orders
+ *
+ * 
+ *
+ * @url http://docs.developer.amazonservices.com/en_US/orders/2013-09-01/index.html
+ * @version 2013-09-01
+ */
+interface OrdersInterface
+{
+}
