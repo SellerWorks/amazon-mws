252cdec072d100952d79bf00d182181bb7af1713
Steve Nebes <snebes@gmail.com>
2016-06-17 14:29:45 -0500

    iniot

--- src/FulfillmentInbound/XmlService.php
+++ src/FulfillmentInbound/XmlService.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace SellerWorks\Amazon\MWS\FulfillmentInbound;
 
-use Closure;
-use ReflectionProperty;
-use Sabre\Xml\Reader;
-use SellerWorks\Amazon\MWS\Common\Entities\ResponseMetadata;
-use SellerWorks\Amazon\MWS\Common\Responses\ErrorResponse;
-use SellerWorks\Amazon\MWS\Common\Responses\GetServiceStatusResponse;
-use SellerWorks\Amazon\MWS\Common\Results\Error;
-use SellerWorks\Amazon\MWS\Common\Results\GetServiceStatusResult;
+use SellerWorks\Amazon\MWS\Common\XmlService as BaseXmlService;
 
 /**
  * Defines how to deserialize xml using Sabre\Xml
  */
-class XmlService extends \Sabre\Xml\Service
+class XmlService extends BaseXmlService
 {
     /**
      * @const string
@@ -30,14 +23,7 @@ class XmlService extends \Sabre\Xml\Service
     {
         $namespace = sprintf('{%s}', static::NS);
 
-        $this->elementMap = [
-            // Common objects.
-            "{$namespace}Error" => $this->mapObject(Error::class),
-            "{$namespace}ErrorResponse" => $this->mapObject(ErrorResponse::class),
-            "{$namespace}GetServiceStatusResponse" => $this->mapObject(GetServiceStatusResponse::class),
-            "{$namespace}GetServiceStatusResult" => $this->mapObject(GetServiceStatusResult::class),
-
-
+        $this->elementMap = array_merge($this->elementMap, [
             // Response objects.
             "{$namespace}CreateInboundShipmentPlanResponse" => $this->mapObject(Responses\CreateInboundShipmentPlanResponse::class),
             "{$namespace}CreateInboundShipmentResponse" => $this->mapObject(Responses\CreateInboundShipmentResponse::class),
@@ -78,7 +64,7 @@ class XmlService extends \Sabre\Xml\Service
 
             // Type objects.
             "{$namespace}Amount" => $this->mapObject(Entities\Amount::class),
-            "{$namespace}ResponseMetadata" => $this->mapObject(ResponseMetadata::class),
+            
             "{$namespace}ShipFromAddress" => $this->mapObject(Entities\Address::class),
             "{$namespace}ShipToAddress" => $this->mapObject(Entities\Address::class),
 
@@ -87,58 +73,6 @@ class XmlService extends \Sabre\Xml\Service
             "{$namespace}PrepInstructionList" => function(Reader $reader) use ($namespace) {
                 return \Sabre\Xml\Deserializer\repeatingElements($reader, "{$namespace}PrepInstruction");
             },
-        ];
-    }
-
-    /**
-     * Return new object by closure.
-     *
-     * @param  string $className
-     * @return Closure
-     */
-    protected function mapObject(string $className): Closure
-    {
-        $namespace = static::NS;
-
-        return function(Reader $reader) use ($namespace, $className) {
-            $object = new $className;
-            $values = \Sabre\Xml\Deserializer\keyValue($reader, $namespace);
-
-            foreach ($values as $property => $value) {
-                if (property_exists($object, $property)) {
-                    $reflection = new ReflectionProperty($object, $property);
-                    $reflection->setAccessible(true);
-                    $reflection->setValue($object, $value);
-                }
-            }
-
-            return $object;
-        };
-    }
-
-    /**
-     * Return new collection by closure.
-     *
-     * @param  string $childElementName
-     * @param  string $className
-     * @return Closure
-     */
-    protected function mapCollectionObject(string $childElementName, string $className): Closure
-    {
-        $namespace = static::NS;
-
-        return function(Reader $reader) use ($childElementName, $className, $namespace) {
-            $elementMap = $reader->elementMap;
-            $elementMap[$childElementName] = $this->mapObject($className);
-            $result = [];
-
-            foreach ($reader->parseGetElements($elementMap) as $element) {
-                if ($element['name'] === $childElementName) {
-                    $result[] = $element['value'];
-                }
-            }
-
-            return $result;
-        };
+        ]);
     }
 }
